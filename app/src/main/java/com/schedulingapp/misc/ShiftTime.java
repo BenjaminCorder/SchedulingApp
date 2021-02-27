0ab58388bbe52a230bb2d0363b4147307936af08
Benjamin Corder <cor16020@byui.edu>
2021-02-26 17:19:21 -0700

    Fixing weird issues.

---
+++ app/src/main/java/com/schedulingapp/misc/ShiftTime.java
@@ -0,0 +1,8 @@
+package com.schedulingapp.misc;
+
+public enum ShiftTime {
+    SHIFT_TIME_UNSPECIFIED,
+    DAYS,
+    SWINGS,
+    MIDS
+}
---
+++ app/src/main/java/com/schedulingapp/presenter/MainPresenter.java
@@ -0,0 +1,11 @@
+package com.schedulingapp.presenter;
+
+public class MainPresenter implements MainContractPresenter{
+    /**
+     *
+     */
+    @Override
+    public void onDestroy() {
+
+    }
+}
