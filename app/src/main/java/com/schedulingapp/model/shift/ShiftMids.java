57360151d497621dccc1f08b28887cc866d4cb77
Benjamin Corder <cor16020@byui.edu>
2021-03-02 20:10:56 -0700

    Updated JavaDoc and added primitive logic to com.schedulingapp.model.shift.ShiftMids

--- app/src/main/java/com/schedulingapp/model/shift/ShiftMids.java
+++ app/src/main/java/com/schedulingapp/model/shift/ShiftMids.java
@@ -1,9 +1,15 @@
 package com.schedulingapp.model.shift;
 
+import com.schedulingapp.misc.Date;
 import com.schedulingapp.misc.ShiftTime;
+import com.schedulingapp.misc.Time;
 
 /**
- * Class ShiftMids
+ * A class representing a midnight shift.
+ *
+ * @author Ben Corder
+ * @version 1.0
+ * @since 1.0
  */
 public class ShiftMids extends Shift {
 
@@ -11,21 +17,41 @@ public class ShiftMids extends Shift {
     // Fields
     //
 
-    static private final ShiftTime shiftTime = ShiftTime.MIDS;
-
     //
     // Constructors
     //
 
+    /**
+     * Non-Default Constructor.
+     *
+     * @param date date of the shift.
+     */
+    public ShiftMids(Date date) {
+        super(date);
+    }
 
+    /**
+     * Non-Default Constructor.
+     *
+     * @param date date of the shift.
+     * @param startTime time the shift begins.
+     * @param endTime time the shift ends.
+     */
+    public ShiftMids(Date date, Time startTime, Time endTime) {
+        super(date);
+    }
+
+    /**
+     * Copy Constructor.
+     *
+     * @param rhs object to make a copy of.
+     */
+    public ShiftMids(ShiftMids rhs) {
+        super(rhs.getDate(), rhs.getStartTime(), rhs.getEndTime());
+    }
 
     //
     // Methods
-    //
-
-
-    //
-    // Accessor methods
     //
 
     /**
@@ -33,16 +59,21 @@ public class ShiftMids extends Shift {
      *
      * @return the value of shiftTime
      */
-    public static ShiftTime getShiftTime() {
-        return shiftTime;
+    @Override
+    public ShiftTime getShiftTime() {
+        return ShiftTime.MIDS;
     }
+
+    //
+    // Accessor methods
+    //
 
     //
     // Other methods
     //
 
     /**
-     *
+     * Ensures that all member variables of the current object are reasonable.
      */
     @Override
     protected void validateShift() {
