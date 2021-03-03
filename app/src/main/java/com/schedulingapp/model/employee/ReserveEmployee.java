eba1ce5987f81e160b3ca03de4c515e35c679788
Benjamin Corder <cor16020@byui.edu>
2021-03-02 19:19:43 -0700

    Updated JavaDoc and added primitive logic to com.schedulingapp.model.employee.ReserveEmployee

--- app/src/main/java/com/schedulingapp/model/employee/ReserveEmployee.java
+++ app/src/main/java/com/schedulingapp/model/employee/ReserveEmployee.java
@@ -1,9 +1,15 @@
 package com.schedulingapp.model.employee;
 
+import com.schedulingapp.misc.EmploymentType;
 import com.schedulingapp.misc.Gender;
 
 /**
  * Class ReserveEmployee
+ *
+ * @author Benjamin Corder
+ * @version 1.0
+ * @since 1.0
+ * @see com.schedulingapp.model.employee.Employee
  */
 public class ReserveEmployee extends Employee {
 
@@ -17,22 +23,48 @@ public class ReserveEmployee extends Employee {
     //
 
     /**
-     * @param name
-     * @param gender
+     * Non-Default Constructor.
+     *
+     * @param firstName first name of employee.
+     * @param lastName last name of employee.
+     * @param gender gender of employee.
      */
-    public ReserveEmployee(String name, Gender gender) {
+    public ReserveEmployee(String firstName, String lastName, Gender gender) {
+        super(firstName, lastName, gender);
     }
 
-
     /**
-     * @param rhs
+     * Copy Constructor.
+     *
+     * @param rhs object to create a copy of.
      */
-    public ReserveEmployee(ReserveEmployee rhs) {
+    public ReserveEmployee(FullTimeEmployee rhs) {
+        super(rhs);
     }
 
     //
     // Methods
     //
+
+    /**
+     * Returns the employment type of the employee.
+     *
+     * @return employment type of employee.
+     */
+    @Override
+    public EmploymentType getEmploymentType() {
+        return EmploymentType.RESERVE;
+    }
+
+    /**
+     * Returns the maximum number of hours the employee can work in a week.
+     *
+     * @return the maximum number of hours the employee can work in a week.
+     */
+    @Override
+    public int getMaxHours() {
+        return 30;
+    }
 
     //
     // Accessor methods
@@ -43,7 +75,7 @@ public class ReserveEmployee extends Employee {
     //
 
     /**
-     *
+     * Ensures that the member variables of the current object make sense.
      */
     @Override
     protected void validate() {
