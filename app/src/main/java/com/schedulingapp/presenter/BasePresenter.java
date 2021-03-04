e0d926a039974450f8644b55129ae4d9d19c4fc0
Ben <ben.redroc@gmail.com>
2021-03-04 16:52:56 -0700

    Updated BasePresenter and BaseView classes.

--- app/src/main/java/com/schedulingapp/presenter/BasePresenter.java
+++ app/src/main/java/com/schedulingapp/presenter/BasePresenter.java
@@ -2,32 +2,26 @@ package com.schedulingapp.presenter;
 
 
 /**
- * Interface BasePresenter
+ * An interface representing all methods shared by presenter classes.
+ *
+ * @author Ben Corder
+ * @version 1.0
+ * @since 1.0
  */
 public interface BasePresenter {
-
     //
-    // Fields
-    //
-
-
-    //
-    // Methods
+    // Activity Methods
     //
 
-
-    //
-    // Accessor methods
-    //
-
-    //
-    // Other methods
-    //
-
-    /**
-     *
-     */
+    void onStart();
+    void onResume();
+    void onPause();
+    void onStop();
+    void onRestart();
     void onDestroy();
 
+    //
+    // Other Methods
+    //
 
 }
--- app/src/main/java/com/schedulingapp/view/BaseView.java
+++ app/src/main/java/com/schedulingapp/view/BaseView.java
@@ -1,32 +1,22 @@
 package com.schedulingapp.view;
 
 /**
- * Interface BaseView
+ * An interface representing all methods shared by a View class.
+ *
+ * @author Ben Corder
+ * @version 1.0
+ * @since 1.0
  */
 public interface BaseView<T> {
-
-    //
-    // Fields
-    //
-
-
     //
     // Methods
     //
 
-
-    //
-    // Accessor methods
-    //
-
-    //
-    // Other methods
-    //
-
     /**
-     * @param presenter
+     * Sets a value for the presenter that corresponds with the view represented by
+     * the current object.
+     *
+     * @param presenter presenter associated with the current object.
      */
     void setPresenter(T presenter);
-
-
 }
